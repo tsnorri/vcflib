5c8b134ddc97266d84c8f7385e4f9090f386e9ab
Tuukka Norri <tsnorri@iki.fi>
2017-01-24 13:44:06 +0200

    Limit number of file descriptors, fix possible double free
    – closeOutputs now clears the list of file pointers in order for delete to be
    called only once.
    – Open files only up to a limit when writing haplotypes.

--- src/vcf2fasta.cpp
+++ src/vcf2fasta.cpp
@@ -83,20 +83,23 @@ map<string, int>& getPloidies(Variant& var, map<string, int>& ploidies, int defa
 }
 
 void closeOutputs(map<string, map<int, SampleFastaFile*> >& outputs) {
-    for (map<string, map<int, SampleFastaFile*> >::iterator f = outputs.begin(); f != outputs.end(); ++f) {
-        for (map<int, SampleFastaFile*>::iterator s = f->second.begin(); s != f->second.end(); ++s) {
-            delete s->second;
-        }
+    for (auto &vPair : outputs)
+    {
+        for (auto &mPair : vPair.second)
+            delete mPair.second;
     }
+    outputs.clear();
 }
 
-void initOutputs(map<string, map<int, SampleFastaFile*> >& outputs, vector<string>& sampleNames, string& seqName, map<string, int>& ploidies, string& prefix) {
+template <typename T>
+void initOutputs(map<string, map<int, SampleFastaFile*> >& outputs, T const &sampleNames, string& seqName, map<string, int>& ploidies, string& prefix) {
     closeOutputs(outputs);
-    for (vector<string>::iterator s = sampleNames.begin(); s != sampleNames.end(); ++s) {
-        map<int, SampleFastaFile*>& outs = outputs[*s];
-        int p = ploidies[*s];
+    for (auto const &sample : sampleNames)
+    {
+        map<int, SampleFastaFile*>& outs = outputs[sample];
+        int p = ploidies[sample];
         for (int i = 0; i < p; ++i) {
-            string name = prefix + *s + "_" + seqName + ":" + convert(i) + ".fasta";
+            string name = prefix + sample + "_" + seqName + ":" + convert(i) + ".fasta";
             if (!outs[i]) {
                 SampleFastaFile* fp = new SampleFastaFile;
                 outs[i] = fp;
@@ -109,87 +112,101 @@ void initOutputs(map<string, map<int, SampleFastaFile*> >& outputs, vector<strin
 
 void vcf2fasta(VariantCallFile& variantFile, FastaReference& reference, string& outputPrefix, int defaultPloidy, string& nullAlleleString) {
     string lastSeq;
-    long int lastPos=0, lastEnd=0;
     map<string, map<int, SampleFastaFile*> > outputs;
     Variant var(variantFile);
     map<string, int> lastPloidies;
-    while (variantFile.getNextVariant(var)) {
-        if (!var.isPhased()) {
-            cerr << "variant " << var.sequenceName << ":" << var.position << " is not phased, cannot convert to fasta" << endl;
-            exit(1);
-        }
-        map<string, int> ploidies;
-        getPloidies(var, ploidies, defaultPloidy);
-        if (var.sequenceName != lastSeq || lastSeq.empty()) {
-            if (!lastSeq.empty()) {
-                string ref5prime = reference.getSubSequence(lastSeq, lastEnd, reference.sequenceLength(lastSeq)-lastEnd);
-                for (map<string, map<int, SampleFastaFile*> >::iterator s = outputs.begin(); s != outputs.end(); ++s) {
-                    map<int, SampleFastaFile*>& f = s->second;
-                    for (map<int, SampleFastaFile*>::iterator o = f.begin(); o != f.end(); ++o) {
-                        o->second->write(ref5prime);
-                    }
-                }
-            }
-            initOutputs(outputs, var.sampleNames, var.sequenceName, ploidies, outputPrefix);
-            lastSeq = var.sequenceName;
-            lastPos = 0;
-        } else if (!lastPloidies.empty() && lastPloidies != ploidies) {
-            cerr << "cannot handle mid-sequence change of ploidy" << endl;
-            // in principle it should be possible...
-            // it's a matter of representation, GFASTA anyone?
-            exit(1);
-        }
-        lastPloidies = ploidies;
-        if (var.position < lastEnd) {
-            cerr << var.position << " vs " << lastEnd << endl;
-            cerr << "overlapping or out-of-order variants at " << var.sequenceName << ":" << var.position << endl;
-            exit(1);
-        }
-        // get reference sequences implied by last->current variant
-        string ref5prime;
-        if (var.position - 1 - lastEnd > 0) {
-            ref5prime = reference.getSubSequence(var.sequenceName, lastEnd, var.position - 1 - lastEnd);
-        }
-        // write alt/ref seqs for current variant based on phased genotypes
-        for (vector<string>::iterator s = var.sampleNames.begin(); s != var.sampleNames.end(); ++s) {
-            string& sample = *s;
-            vector<int> gt = decomposePhasedGenotype(var.getGenotype(sample));
-            // assume no-call == ref?
-            if (gt.empty()) {
-                cerr << "empty genotype for sample " << *s << " at " << var.sequenceName << ":" << var.position << endl;
+    
+    // Handle samples in chunks.
+    auto const &allSamples = variantFile.sampleNames;
+    size_t chunkSize = 200;
+    set<string> currentSamples;
+    for (size_t i = 0, count = allSamples.size(); i < count; i += chunkSize)
+    {
+        long int lastPos=0, lastEnd=0;
+        currentSamples.clear();
+        lastSeq.clear();
+        auto begin = allSamples.cbegin() + i;
+        auto end = allSamples.cbegin() + std::min(i + chunkSize, count);
+        std::copy(begin, end, std::inserter(currentSamples, currentSamples.end()));
+
+        variantFile.reset();
+        while (variantFile.getNextVariant(var)) {
+            if (!var.isPhased()) {
+                cerr << "variant " << var.sequenceName << ":" << var.position << " is not phased, cannot convert to fasta" << endl;
                 exit(1);
             }
-            int i = 0;
-            for (vector<int>::iterator g = gt.begin(); g != gt.end(); ++g, ++i) {
-                // @TCC handle uncalled genotypes (*g == NULL_ALLELE)
-                if( *g == NULL_ALLELE ){
-                    if( nullAlleleString == "" ){
-                        cerr << "empty genotype call for sample " << *s << " at " << var.sequenceName << ":" << var.position << endl;
-                        cerr << "use -n option to set value to output for missing calls" << endl; 
-                        exit(1);
-                    }else{
-                        outputs[sample].at(i)->write(nullAlleleString);
+            map<string, int> ploidies;
+            getPloidies(var, ploidies, defaultPloidy);
+            if (var.sequenceName != lastSeq || lastSeq.empty()) {
+                if (!lastSeq.empty()) {
+                    string ref5prime = reference.getSubSequence(lastSeq, lastEnd, reference.sequenceLength(lastSeq)-lastEnd);
+                    for (map<string, map<int, SampleFastaFile*> >::iterator s = outputs.begin(); s != outputs.end(); ++s) {
+                        map<int, SampleFastaFile*>& f = s->second;
+                        for (map<int, SampleFastaFile*>::iterator o = f.begin(); o != f.end(); ++o) {
+                            o->second->write(ref5prime);
+                        }
                     }
-                }else{
-                    outputs[sample].at(i)->write(ref5prime+var.alleles.at(*g));
+                }
+                initOutputs(outputs, currentSamples, var.sequenceName, ploidies, outputPrefix);
+                lastSeq = var.sequenceName;
+                lastPos = 0;
+            } else if (!lastPloidies.empty() && lastPloidies != ploidies) {
+                cerr << "cannot handle mid-sequence change of ploidy" << endl;
+                // in principle it should be possible...
+                // it's a matter of representation, GFASTA anyone?
+                exit(1);
+            }
+            lastPloidies = ploidies;
+            if (var.position < lastEnd) {
+                cerr << var.position << " vs " << lastEnd << endl;
+                cerr << "overlapping or out-of-order variants at " << var.sequenceName << ":" << var.position << endl;
+                exit(1);
+            }
+            // get reference sequences implied by last->current variant
+            string ref5prime;
+            if (var.position - 1 - lastEnd > 0) {
+                ref5prime = reference.getSubSequence(var.sequenceName, lastEnd, var.position - 1 - lastEnd);
+            }
+            // write alt/ref seqs for current variant based on phased genotypes
+            for (auto const &sample : currentSamples) {
+                vector<int> gt = decomposePhasedGenotype(var.getGenotype(sample));
+                // assume no-call == ref?
+                if (gt.empty()) {
+                    cerr << "empty genotype for sample " << sample << " at " << var.sequenceName << ":" << var.position << endl;
+                    exit(1);
+                }
+                int i = 0;
+                for (vector<int>::iterator g = gt.begin(); g != gt.end(); ++g, ++i) {
+                    // @TCC handle uncalled genotypes (*g == NULL_ALLELE)
+                    if( *g == NULL_ALLELE ){
+                        if( nullAlleleString == "" ){
+                            cerr << "empty genotype call for sample " << sample << " at " << var.sequenceName << ":" << var.position << endl;
+                            cerr << "use -n option to set value to output for missing calls" << endl; 
+                            exit(1);
+                        }else{
+                            outputs[sample].at(i)->write(nullAlleleString);
+                        }
+                    }else{
+                        outputs[sample].at(i)->write(ref5prime+var.alleles.at(*g));
+                    }
+                }
+            }
+            lastPos = var.position - 1;
+            lastEnd = lastPos + var.ref.size();
+        }
+        // write last sequences
+        {
+            string ref5prime = reference.getSubSequence(lastSeq, lastEnd, reference.sequenceLength(lastSeq)-lastEnd);
+            for (map<string, map<int, SampleFastaFile*> >::iterator s = outputs.begin(); s != outputs.end(); ++s) {
+                map<int, SampleFastaFile*>& f = s->second;
+                for (map<int, SampleFastaFile*>::iterator o = f.begin(); o != f.end(); ++o) {
+                    o->second->write(ref5prime);
                 }
             }
         }
-        lastPos = var.position - 1;
-        lastEnd = lastPos + var.ref.size();
+        closeOutputs(outputs);
+        // outputs are closed by ~SampleFastaFile
     }
-    // write last sequences
-    {
-        string ref5prime = reference.getSubSequence(lastSeq, lastEnd, reference.sequenceLength(lastSeq)-lastEnd);
-        for (map<string, map<int, SampleFastaFile*> >::iterator s = outputs.begin(); s != outputs.end(); ++s) {
-            map<int, SampleFastaFile*>& f = s->second;
-            for (map<int, SampleFastaFile*>::iterator o = f.begin(); o != f.end(); ++o) {
-                o->second->write(ref5prime);
-            }
-        }
-    }
-    closeOutputs(outputs);
-    // outputs are closed by ~SampleFastaFile
 }
 
 int main(int argc, char** argv) {
